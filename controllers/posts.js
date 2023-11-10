e89372dc44e4eae9490541db731bcedc20ae5acd
KevinZucca <kev.94@live.it>
2023-11-10 15:40:19 +0100

    feat: html and json results on page through controllers added

---
+++ controllers/posts.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const jsonPosts = require("../posts.json");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function index(req, res) {
+  res.format({
+    html: () => {
+      const posts = fs.readFileSync(
+        path.resolve(__dirname, "../layouts/posts.html"),
+        "utf-8"
+      );
+
+      //   editing html file with main content
+      const htmlContent = [];
+
+      htmlContent.push("<ul>");
+
+      for (const post of jsonPosts) {
+        htmlContent.push(
+          `<li><h3>${post.title}</h3> <br> ${post.content} <br> <img src='${post.src}' style="width= 200px"> </li>`
+        );
+      }
+
+      htmlContent.push("</ul>");
+      const joinedHtml = htmlContent.join("");
+      const finalContent = posts.replace("@content", joinedHtml);
+
+      res.type("html").send(finalContent);
+    },
+    json: () => {
+      res.type("json").send(jsonPosts);
+    },
+    default: () => {
+      res.status(406).send("Type not valid");
+    },
+  });
+}
+
+module.exports = { index };
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const homeController = require("./controllers/home");
+const postsController = require("./controllers/posts");
+
 const port = process.env.PORT;
 
+// ROUTES
 app.get("/", homeController.index);
+app.get("/posts", postsController.index);
 
+//SERVER LISTEN
 app.listen(port || 8000, () => {
   console.log(`Server is running on port ${port}`);
 });
