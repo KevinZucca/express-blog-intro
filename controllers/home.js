bdd79b9ffc2f55f0e8088688f1dead3fbad08e0d
KevinZucca <kev.94@live.it>
2023-11-10 14:24:23 +0100

    feat: created layouts folder, added correct route to html file

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
 function index(req, res) {
-  res.send("hello world");
+  res.format({
+    html: () => {
+      const homePage = fs.readFileSync(
+        path.resolve(__dirname, "../layouts/homepage.html"),
+        "utf-8"
+      );
+
+      res.type("html").send(homePage);
+    },
+  });
 }
 
 module.exports = { index };
